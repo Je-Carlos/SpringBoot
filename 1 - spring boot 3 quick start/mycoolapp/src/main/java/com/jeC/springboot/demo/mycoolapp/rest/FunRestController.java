60e03ef41b0b5ae8d0b04119dabe0e20d2417437
Jean Carlos <cpjeanberg@gmail.com>
2024-03-27 20:05:11 -0300

    testing how the application.controller works

--- 1 - spring boot 3 quick start/mycoolapp/src/main/java/com/jeC/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 1 - spring boot 3 quick start/mycoolapp/src/main/java/com/jeC/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.jeC.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,10 @@ public class FunRestController {
     public String sayHello(){
         return "Hello World!";
     }
+
+    // read data from: application.properties
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
 }
